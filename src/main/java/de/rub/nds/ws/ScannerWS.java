4c79444c7034416ea65506022e932f538b76a440
ic0ns <robert.merget@rub.de>
2017-03-22 17:23:19 +0100

    Moved classes to TLS-Attacker for better code reused and just kept webservice here

--- src/main/java/de/rub/nds/ws/ScannerWS.java
+++ src/main/java/de/rub/nds/ws/ScannerWS.java
@@ -3,7 +3,8 @@
  *
  * Copyright 2014-2016 Ruhr University Bochum / Hackmanit GmbH
  *
- * Licensed under Apache License 2.0 http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  */
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -12,8 +13,8 @@
  */
 package de.rub.nds.ws;
 
-import de.rub.nds.tlsscanner.Report.SiteReport;
 import de.rub.nds.tlsscanner.TLSScanner;
+import de.rub.nds.tlsscanner.report.SiteReport;
 import java.util.concurrent.ExecutorService;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -40,7 +41,7 @@ public class ScannerWS {
     @GET
     @Produces("application/json")
     @Path("{host}")
-    public String getJson(@PathParam("host")String host) {
+    public String getJson(@PathParam("host") String host) {
         TLSScanner scanner = new TLSScanner(host);
         SiteReport report = scanner.scan();
         return report.getJsonReport();
